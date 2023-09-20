bd4e74fadeaa161b8d33c9721c5761c958b97358
JulienMnt <julien.menet@edu.devinci.fr>
2023-09-20 16:54:06 +0200

    Cours Mercredi

--- Cours 4 (Boucle)/boucle.js
+++ Cours 4 (Boucle)/boucle.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let tableaufruit = ["Fraise", "Mangue", "Goyave"];
+console.log(tableaufruit);
+console.log(tableaufruit.length); //Taille du tableau
+console.log(tableaufruit[0]);
+tableaufruit[1]= "Framboise";
+tableaufruit.push("Cerise");
+
+for(let i = 0; i < tableaufruit.length ; i++){
+    console.log(tableaufruit[i]);
+}
---
+++ Exercices/Exercice Mercredi 2/ex.js
@@ -0,0 +1,6 @@
+"use strict";
+
+for(let i=11; i>0;i--){
+    if(i>1){console.log(i-1);}
+    else if(i=1){console.log("Bonne année");}}
+    
